7b2134a8e4bca15a1bf964396401791e832a2ab5
Vitalii Yakubovskyi <vitaliiyakubovskyi@gmail.com>
2024-09-06 01:13:58 +0200

    new

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+// Heaader
+const modal = document.querySelector('.header_navigation_window');
+const closeButton = document.querySelector('.burger_icon_close');
+const burgerButton = document.querySelector('.button_burger');
+
+
+if (!modal.classList.contains('to_close')) {
+  modal.classList.add('to_close');
+}
+
+closeButton.addEventListener('click', function () {
+  modal.classList.add('to_close');
+});
+
+burgerButton.addEventListener('click', function () {
+  modal.classList.remove('to_close');
+});
+
+
+
+
 // TABS
 const tabsBtns = document.querySelectorAll(".tabs__nav button");
 const tabsItems = document.querySelectorAll(".tabs__item");
