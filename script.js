78e6c2f278d822eb165a62250abd501b3c28e574
Vitalii Yakubovskyi <vitaliiyakubovskyi@gmail.com>
2024-09-11 02:18:17 +0200

    mobile-tablet-pc

--- script.js
+++ script.js
@@ -19,7 +19,7 @@
   // Закрываем мобильное меню на более широких экранах
   // в случае изменения ориентации устройства.
   // Close the mobile menu on wider screens if the device orientation changes
-  window.matchMedia("(min-width: 768px)").addEventListener("change", (e) => {
+  window.matchMedia("(min-width: 1279px)").addEventListener("change", (e) => {
     if (!e.matches) return;
     mobileMenu.classList.remove("is-open");
     openMenuBtn.setAttribute("aria-expanded", false);
